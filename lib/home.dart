71f643f0633d64a134d7f7c3bae0017df22eb7c2
Roopesh <roopesh.rai@plutustec.com>
2022-11-24 19:20:02 +0530

    third

--- lib/home.dart
+++ lib/home.dart
@@ -160,69 +160,155 @@ class _HomeState extends State<Home> {
         SizedBox(
           height: 10,
         ),
-        Container(
-          padding: EdgeInsets.all(15.0),
-          child: GridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            shrinkWrap: true,
-            children: List.generate(
-              2,
-              (index) {
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/rsd_25.jpg"),
-                        fit: BoxFit.cover,
+        SingleChildScrollView(
+          child: Container(
+            padding:
+                EdgeInsets.only(left: 5.0, right: 5.0, bottom: 10, top: 10),
+            child: SingleChildScrollView(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 0.0,
+                shrinkWrap: true,
+                children: List.generate(
+                  2,
+                  (index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/rsd_25.jpg",
+                              height: 125,
+                            ),
+                            Center(
+                                child: Text(
+                              "Let's Launch\n 30 Minutes",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                            ))
+                          ],
+                        ),
                       ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                    ),
-                    // child: Text("Let's Launch 30 Minutes"),
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+              ),
             ),
           ),
         ),
-        Container(
-          padding: EdgeInsets.all(15.0),
-          child: GridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            shrinkWrap: true,
-            children: List.generate(
-              2,
+        SingleChildScrollView(
+          child: Container(
+            padding:
+                EdgeInsets.only(left: 5.0, right: 5.0, bottom: 10, top: 10),
+            child: SingleChildScrollView(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 0.0,
+                shrinkWrap: true,
+                children: List.generate(
+                  2,
                   (index) {
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/rsd_b.png"),
-                        fit: BoxFit.cover,
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/rsd_b.png",
+                              height: 125,
+                            ),
+                            Center(
+                                child: Text(
+                              "Unicorn Dance\n 45 Minutes",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                            ))
+                          ],
+                        ),
                       ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                    ),
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+              ),
             ),
           ),
-        )
+        ),
       ]),
     ),
     Text('Profile Page',
         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Notifications Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    SingleChildScrollView(
+        child: Container(
+      width: 500,
+      height: 500,
+      padding: new EdgeInsets.only(right: 1.0, left: 1.0, bottom: 30, top: 20),
+      child: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            color: Colors.white,
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: 44,
+                      minHeight: 44,
+                      maxWidth: 64,
+                      maxHeight: 64,
+                    ),
+                    child: Image.asset("assets/rsd_logo.png"),
+                  ),
+                  title: Text('\nNEW TAP PLAY LIST ADDED',
+                      style: TextStyle(
+                          fontSize: 17.0, fontWeight: FontWeight.bold)),
+                  subtitle: Text(
+                      '\nWe are super excited to announce that we have added a brand new playlist with new tracks to Ready Set Tap\n',
+                      style: TextStyle(fontSize: 12.0)),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10,),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            color: Colors.white,
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: 44,
+                      minHeight: 44,
+                      maxWidth: 64,
+                      maxHeight: 64,
+                    ),
+                    child: Image.asset("assets/rsd_logo.png"),
+                  ),
+                  title: Text('\nUPDATE 2.4 AVAILABLE',
+                      style: TextStyle(
+                          fontSize: 17.0, fontWeight: FontWeight.bold)),
+                  subtitle: Text(
+                      '\nWe are super excited to announce that we have added a brand new playlist with new tracks to Ready Set Tap\n',
+                      style: TextStyle(fontSize: 12.0)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    )),
     Text('Profile Page',
         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
   ];
@@ -236,6 +322,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 140, 0, 149),
         toolbarHeight: 150.00, //set your height
@@ -290,9 +377,9 @@ class _HomeState extends State<Home> {
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
           elevation: 5),
-      body: Container(
-        padding: EdgeInsets.only(top: 15, left: 15, right: 15),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(top: 15, left: 15, right: 15),
           child: Column(
             children: [_widgetOptions.elementAt(_selectedIndex)],
           ),
